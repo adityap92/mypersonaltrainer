9f179dae6400530491e288aae3dbf71c6e9564d6
aditya <adityap92@gmail.com>
2017-07-10 10:30:09 -0700

    changed activity to load from fragment

--- app/src/main/java/io/mypersonaltrainer/mypersonaltrainer/MainActivity.java
+++ app/src/main/java/io/mypersonaltrainer/mypersonaltrainer/MainActivity.java
@@ -1,13 +1,16 @@
 package io.mypersonaltrainer.mypersonaltrainer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView tv_hello_world;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +18,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        tv_hello_world = (TextView) findViewById(R.id.tv_hello_world);
 
-        tv_hello_world.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tv_hello_world.setText("asdf");
-            }
-        });
 
+        LoginFragment loginFrag = new LoginFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.container, loginFrag, "LoginFragment")
+                .addToBackStack(null)
+                .commit();
+
+    }
+
+    public static class LoginFragment extends Fragment{
+
+        TextView tv_hello_world;
+
+        public LoginFragment(){}
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+            tv_hello_world = (TextView) rootView.findViewById(R.id.tv_hello_world);
+
+            tv_hello_world.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    tv_hello_world.setText("asdf");
+                }
+            });
+
+            return rootView;
+        }
     }
 }

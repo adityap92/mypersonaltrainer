26594e34c67a50422146891dce1276bb2d2aa0de
aditya <adityap92@gmail.com>
2017-07-11 14:57:14 -0700

    added WorkoutFragment with basic UI

--- app/src/main/java/io/mypersonaltrainer/mypersonaltrainer/MainActivity.java
+++ app/src/main/java/io/mypersonaltrainer/mypersonaltrainer/MainActivity.java
@@ -1,24 +1,45 @@
 package io.mypersonaltrainer.mypersonaltrainer;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    static GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
+    public final String TAG = MainActivity.class.getSimpleName();
+    public Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = getApplicationContext();
 
         if(savedInstanceState==null){
             LoginFragment loginFrag = new LoginFragment();
@@ -28,13 +49,84 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
         }
 
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                } /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        OptionalPendingResult<GoogleSignInResult> opr = Auth
+                .GoogleSignInApi.silentSignIn(mGoogleApiClient);
+
+        if(opr.isDone()){
+            Log.d(TAG, "Sign in was cached");
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        }else{
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+
+    public void openBioFragment(){
+
+        BioFragment bioFragment = new BioFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, bioFragment, "BioFragment")
+                .commit();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            //mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            Toast.makeText(mContext,"Name: " + acct.getDisplayName(), Toast.LENGTH_SHORT).show();
+        } else {
+            // Signed out, show unauthenticated UI.
+            Toast.makeText(mContext,"Not Logged in", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public static class LoginFragment extends Fragment{
 
-        @BindView(R.id.bLogin)
-        Button bLogin;
+        @BindView(R.id.sign_in_button)
+        SignInButton bLogin;
 
         public LoginFragment(){}
 
@@ -45,20 +137,25 @@ public class MainActivity extends AppCompatActivity {
             View rootView = inflater.inflate(R.layout.fragment_login, container, false);
             ButterKnife.bind(this, rootView);
 
+
+
             bLogin.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    BioFragment bioFragment = new BioFragment();
+                    signIn();
 
-                    FragmentManager fragmentManager = getFragmentManager();
-                    fragmentManager.beginTransaction()
-                            .replace(R.id.container, bioFragment, "BioFragment")
-                            .commit();
                 }
             });
 
 
             return rootView;
         }
+
+        public void signIn() {
+            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        }
     }
+
+
 }
--- app/src/main/java/io/mypersonaltrainer/mypersonaltrainer/PlannerFragment.java
+++ app/src/main/java/io/mypersonaltrainer/mypersonaltrainer/PlannerFragment.java
@@ -3,9 +3,15 @@ package io.mypersonaltrainer.mypersonaltrainer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by aditya on 7/10/17.
@@ -13,13 +19,36 @@ import android.view.ViewGroup;
 
 public class PlannerFragment extends Fragment {
 
+    @BindView(R.id.tvMonday)
+    TextView tvMonday;
+    private Unbinder unbinder;
+
     public PlannerFragment(){}
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_planner, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        tvMonday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                WorkoutFragment workoutFragment = new WorkoutFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, workoutFragment)
+                        .commit();
+            }
+        });
 
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
